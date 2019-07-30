7ded434142987c2d828d34dbe1e04e594c6cd13c
Paul Atkins <patkins2@uncc.edu>
2019-07-30 16:22:02 -0400

    Initial Commit
    Building is in scene. Basic rotation implemented. Also a few test objects for opening webpages are in the scene.

---
+++ Woodward Guide TEST/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float zoomSpeed = 20;
+
+    public Camera camera;
+
+    private void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if(scroll != 0.0f)
+            //camera.fieldOfView += scroll * zoomSpeed;
+            transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+    }
+}
---
+++ Woodward Guide TEST/Assets/Scripts/WebURLScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebURLScript : MonoBehaviour
+{
+   
+    public void OnMouseDown()
+    {
+        if (gameObject.name == "VisLab")
+        {
+            Application.OpenURL("www.viscenter.uncc.edu");
+        }
+        else
+        {
+            Application.OpenURL("www.uncc.edu");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
